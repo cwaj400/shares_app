2600025d8206c1a6331900fe1f3f84d0cdcb9603
Will Angell-James <william.angelljames9@gmail.com>
2018-08-28 13:58:50 -0400

    inital commit

---
+++ handlers/win-handle.js
@@ -0,0 +1,11 @@
+const $ = require('jquery');
+const {remote} = require('electron');
+var win = remote.getCurrentWindow();
+
+$('#minimize').click(function(){
+    win.minimize();
+});
+
+$('#close').click(function(){
+    win.close();
+});
